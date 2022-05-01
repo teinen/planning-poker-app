71f8adb9447a1d7bda0dbe94d598a5ca254a5ea9
Akira Kanno <teinen.github@gmail.com>
2022-05-01 15:51:50 +0900

    Add some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
 import { db } from './firebase'
 
-import CreateRoomButton from './components/CreateRoomButton'
+import Top from './pages/Top'
+import Room from './pages/Room'
 
 function App() {
   console.log(db)
 
   return (
     <div className="App">
-      <CreateRoomButton />
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
     </div>
   )
 }
--- src/components/CreateRoomButton.tsx
+++ src/components/CreateRoomButton.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import {
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  serverTimestamp,
+} from 'firebase/firestore'
 import { db } from '../firebase'
 
 const CreateRoomButton: React.FC = () => {
+  const navigate = useNavigate()
+
   const clickHandler = async () => {
     try {
-      const newRoomRef = await addDoc(collection(db, 'rooms'), {
+      const addDocRef = await addDoc(collection(db, 'rooms'), {
         roomId: uuidv4(),
         active: true,
         createdAt: serverTimestamp(),
       })
-      console.log('Room is successfully created. Room ID is ', newRoomRef)
+
+      const docRef = doc(db, 'rooms', addDocRef.id)
+      const docSnap = await getDoc(docRef)
+
+      navigate(`/room/${docSnap.data()?.roomId}`)
     } catch (err) {
       console.log('Room creation is failed.')
     }

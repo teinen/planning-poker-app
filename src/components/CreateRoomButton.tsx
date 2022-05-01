e0bcf8d322642fbc6391aa03942a4fb3feeed936
Akira Kanno <teinen.github@gmail.com>
2022-05-01 15:17:08 +0900

    Add create new room function

--- src/components/CreateRoomButton.tsx
+++ src/components/CreateRoomButton.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import { v4 as uuidv4 } from 'uuid'
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+
 const CreateRoomButton: React.FC = () => {
-  const clickHandler = () => {
-    console.log('clicked')
+  const clickHandler = async () => {
+    try {
+      const newRoomRef = await addDoc(collection(db, 'rooms'), {
+        roomId: uuidv4(),
+        active: true,
+        createdAt: serverTimestamp(),
+      })
+      console.log('Room is successfully created. Room ID is ', newRoomRef)
+    } catch (err) {
+      console.log('Room creation is failed.')
+    }
   }
 
   return <button onClick={clickHandler}>Create a new room</button>

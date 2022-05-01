01b8ad799d5c0537a55730a434480fd289cf3527
Akira Kanno <teinen.github@gmail.com>
2022-05-01 18:29:48 +0900

    Implement joining room logic

---
+++ src/components/JoinRoomButton.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+  useDisclosure,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from '@chakra-ui/react'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase'
+
+const JoinRoomButton: React.FC = () => {
+  const navigate = useNavigate()
+
+  const [roomIdInput, setRoomIdInput] = useState('')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const handleRoomIdInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomIdInput(e.target.value)
+  }
+
+  const handleJoinRoomButtonClick = async () => {
+    try {
+      const docRef = doc(db, 'rooms', roomIdInput)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        navigate(`/room/${docSnap.id}`)
+      } else {
+        window.alert('Room does not exist.')
+      }
+    } catch (error) {
+      console.log('Failed to get room info.')
+    }
+  }
+
+  return (
+    <>
+      <Button colorScheme="cyan" variant="outline" onClick={onOpen}>
+        Join room
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Join existing room</ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody>
+            <FormControl isRequired>
+              <FormLabel htmlFor="room-id">Room id</FormLabel>
+              <Input
+                id="room-id"
+                placeholder="Room id"
+                value={roomIdInput}
+                onChange={handleRoomIdInputChange}
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel htmlFor="nickname">Nickname</FormLabel>
+              <Input id="nickname" placeholder="Nickname" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant="ghost" mr={3} onClick={onClose}>
+              cancel
+            </Button>
+
+            <Button
+              colorScheme="cyan"
+              color="white"
+              onClick={handleJoinRoomButtonClick}
+            >
+              Join
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default JoinRoomButton
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import CreateRoomButton from '../components/CreateRoomButton'
+import JoinRoomButton from '../components/JoinRoomButton'
 
 const Top: React.FC = () => {
   return (
     <div>
       Top Page
       <br />
+      <br />
       <CreateRoomButton />
+      <br />
+      <br />
+      <JoinRoomButton />
     </div>
   )
 }

df2f20d78d10f2ef2d5077d3888e582612e74227
Akira Kanno <teinen.github@gmail.com>
2022-05-09 00:54:14 +0900

    Add functions

--- src/components/OwnerControls.tsx
+++ src/components/OwnerControls.tsx
@@ -1,9 +1,52 @@
 import { Button, Tooltip } from '@chakra-ui/react'
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  writeBatch,
+} from 'firebase/firestore'
 import React from 'react'
+import { useMatch, useNavigate } from 'react-router-dom'
+
+import { db } from '../firebase'
 
 const OwnerControls: React.FC = () => {
+  const navigate = useNavigate()
+
+  const match = useMatch('/room/:roomId')
+  const roomId = match?.params.roomId
+
+  if (typeof roomId === 'undefined') {
+    throw new Error()
+  }
+
   const handleResetButtonClick = async () => {
-    console.log('Click Reset')
+    const batch = writeBatch(db)
+
+    const q = query(collection(db, 'rooms', roomId, 'participants'))
+    const querySnapshot = await getDocs(q)
+
+    querySnapshot.docs.forEach((doc) => {
+      batch.set(doc.ref, { ...doc.data(), estimate: '' })
+    })
+
+    await batch.commit()
+  }
+
+  const handleCloseButtonClick = async () => {
+    try {
+      const roomDocRef = doc(db, 'rooms', roomId)
+
+      await updateDoc(roomDocRef, {
+        active: false,
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log('Close room is failed')
+    }
   }
 
   return (
@@ -15,7 +58,12 @@ const OwnerControls: React.FC = () => {
       </Tooltip>
 
       <Tooltip hasArrow label="Close this room, when planning has finished">
-        <Button variant="outline" colorScheme="red" ml="16px">
+        <Button
+          variant="outline"
+          colorScheme="red"
+          ml="16px"
+          onClick={handleCloseButtonClick}
+        >
           Close
         </Button>
       </Tooltip>

f7199fab27d390bc32f858c47000a4aff2c6b3d2
Akira Kanno <teinen.github@gmail.com>
2022-06-04 20:19:31 +0900

    Chore fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Container } from '@chakra-ui/react'
+import { css } from '@emotion/react'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
-
-import './App.css'
 
 import Room from './pages/Room'
 import Top from './pages/Top'
 
 function App() {
+  const AppStyle = css`
+    text-align: center;
+  `
+
   return (
-    <div className="App">
+    <div css={AppStyle}>
       <RecoilRoot>
         <Container padding="16px" maxW="1124">
           <Routes>

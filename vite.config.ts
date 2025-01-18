d38037e22e6c4e98b4dd491f3da7163e9b3a9206
Akira Kanno <teinen.github@gmail.com>
2025-01-18 15:52:02 +0900

    format 第一弾

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     open: true,
   },
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
   plugins: [
     react({
-      jsxImportSource: '@emotion/react',
+      jsxImportSource: "@emotion/react",
     }),
   ],
-})
+});
